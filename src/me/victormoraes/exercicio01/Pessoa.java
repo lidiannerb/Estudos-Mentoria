511d7efe05b8595e4f6b71516f158e1cd2b5c963
Lidianne Romão <83047245+lidiannerb@users.noreply.github.com>
2021-05-15 17:03:33 -0300

    Exercicio 1 feito

--- src/me/victormoraes/exercicio01/Pessoa.java
+++ src/me/victormoraes/exercicio01/Pessoa.java
@@ -1,5 +1,14 @@
 package me.victormoraes.exercicio01;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Crie uma classe para representar uma pessoa, com os atributos privados de nome, data de
  * nascimento e altura. Crie os métodos públicos necessários para sets e gets e também um
@@ -8,10 +17,53 @@ package me.victormoraes.exercicio01;
  */
 public class Pessoa {
 
-    public void imprimirDados() {
+    private String nome;
+    private String altura;
+    private String dataNasc;
+
+    public String getAltura() {
+        return altura;
     }
 
-    public int calcularIdade() {
-        return 0;
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void imprimirDados() {
+        System.out.println("nome: "+this.nome + "altura: "+ this.altura + "Data de nascimento: " + this.dataNasc);
+    }
+
+    public int calcularIdade() throws ParseException {
+        String idade = this.dataNasc;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date d = sdf.parse(idade);
+        Calendar c = Calendar.getInstance();
+        c.setTime(d);
+
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH) +1;
+        int date = c.get(Calendar.DATE);
+
+        LocalDate ll = LocalDate.of(year, month, date);
+        LocalDate now1 = LocalDate.now();
+        Period diffl = Period.between(ll, now1);
+
+        return diffl.getYears();
     }
 }
--- src/me/victormoraes/exercicio01/TestePessoa.java
+++ src/me/victormoraes/exercicio01/TestePessoa.java
@@ -1,12 +1,18 @@
 package me.victormoraes.exercicio01;
 
+import java.text.ParseException;
 import java.time.LocalDate;
 
 public class TestePessoa {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // Testar o método imprimirDados
+        Pessoa pessoa = new Pessoa();
+        pessoa.setAltura("1,62");
+        pessoa.setDataNasc("02/04/1982");
+        pessoa.setNome("Lidianne");
 
-        // Testar o método calcularIdade
+        pessoa.imprimirDados();
+        System.out.println(pessoa.calcularIdade());
 
     }
 }
